620f35a902778a7a65bdd5db379131e8090a6d8a
matana <matana@cynet.com>
2021-05-07 03:16:32 +0300

    updated docs and consts.py

--- documnets.py
+++ documnets.py
@@ -4,7 +4,8 @@ from mongoengine import *
 
 class Posts(Document):
     username = StringField(required=True)
-    creation_date = DateField(required=True, default=datetime.now)
+    creation_date = DateField(required=True, default=datetime.now().isoformat)
     title = StringField(required=True)
     body = StringField(required=True)
     comments = ListField(required=True)
+    image_path = StringField(required=True)
